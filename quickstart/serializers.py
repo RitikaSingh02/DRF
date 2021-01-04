937ff10e85b167896591fb853f9391feda57b66b
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-04 23:30:15 +0530

    changes fields in serialzers

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -2,35 +2,45 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Article
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-    
+
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
 
-#using serializser class
+# using serializser class
+
+
 class ArticleSerialiser(serializers.Serializer):
-    title=serializers.CharField(max_length=100)
-    author=serializers.CharField(max_length=100)
-    email=serializers.CharField(max_length=100)
-    date=serializers.CharField(max_length=100)
-    
-    def create(self,validated_data):
+    title = serializers.CharField(max_length=100)
+    author = serializers.CharField(max_length=100)
+    email = serializers.CharField(max_length=100)
+    date = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
         return Article.objects.create(**validated_data)
-    def update(self,instance,validated_data):
-        instance.title=validated_data.get('title',instance.title)
-        instance.author=validated_data.get('author',instance.author)
-        instance.email=validated_data.get('email',instance.email)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.email = validated_data.get('email', instance.email)
         instance.save()
         return instance
 
-#using Modelserializer class
+# using Modelserializer class
+
+
 class ArticleModelSerialiser(serializers.ModelSerializer):
     class Meta:
-        model=Article
-        fields=['title','author','email','date']
+        model = Article
+        fields = '__all__'
+        # list_serializer_data = ArticleModelSerialiser
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
