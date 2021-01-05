865cd78cf2df38ff25d05bea2ffd11c1b01935d0
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-06 00:43:20 +0530

    added status app url

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,30 +1,32 @@
 from django.urls import include, path
 from django.contrib import admin
 from rest_framework import routers
-#https://www.django-rest-framework.org/api-guide/routers/
+# https://www.django-rest-framework.org/api-guide/routers/
 from quickstart import views
 from quickstart.views import ArticleViewSet
 from quickstart import urls
 
 # REST framework adds support for automatic URL routing to Django(similar to rails),
-# and provides you with a simple, 
+# and provides you with a simple,
 # quick and consistent way of wiring your view logic to a set of URLs.
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)#register(prefix,viewsets,basename)#basename is optinal
+# register(prefix,viewsets,basename)#basename is optinal
+router.register(r'users', views.UserViewSet)
 # The basename argument is used to specify the initial part of the view name pattern. In the example above,
 # that's the users or groups part.
 router.register(r'groups', views.GroupViewSet)
-router.register(r'articles',views.ArticleViewSet,basename="articles")#basename attribute is compulsory if the viewsetdoes not have a queryset attribute
-##error like 
-    #::AssertionError: `basename` argument not specified, and could not automatically determine the name from the viewset, as it does not have a `.queryset` attribute
-    #is generated
-router.register(r'new-api',views.UserNewViewSet,basename="new")
+# basename attribute is compulsory if the viewsetdoes not have a queryset attribute
+router.register(r'articles', views.ArticleViewSet, basename="articles")
+# error like
+#::AssertionError: `basename` argument not specified, and could not automatically determine the name from the viewset, as it does not have a `.queryset` attribute
+# is generated
+router.register(r'new-api', views.UserNewViewSet, basename="new")
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
- #this creates us three form of url pattern from our urls
+    # this creates us three form of url pattern from our urls
     # http://localhost:8000/
     #     ^$ [name='api-root']
     #     ^users/$ [name='user-list']
@@ -39,8 +41,9 @@ urlpatterns = [
     #     ^new\.(?P<format>[a-z0-9]+)/?$ [name='user-list']
     #     ^new/(?P<pk>[^/.]+)/$ [name='user-detail']
     #     ^new/(?P<pk>[^/.]+)\.(?P<format>[a-z0-9]+)/?$ [name='user-detail']
-    
+
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/',admin.site.urls),
-    path('app/',include('quickstart.urls')),
+    path('admin/', admin.site.urls),
+    path('app/', include('quickstart.urls')),
+    path('status/', include('status.urls')),
 ]
