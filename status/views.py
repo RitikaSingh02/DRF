b0e9d4ef639515706b85b71d201c01de3032873e
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-06 00:44:30 +0530

    added basic APIView

--- status/views.py
+++ status/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from status.serializers.serializers import StatusSerializer
+from .models import Status
+
+
+class StatusViewset(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, format=None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+
 
 # Create your views here.
