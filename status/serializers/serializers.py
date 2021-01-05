76c97099c0b5970024e42921a48f07ac378cb2b3
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-06 00:45:00 +0530

    added basic comments

--- status/serializers/serializers.py
+++ status/serializers/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from ..models import Status
 
 
+# serializer = SnippetSerializer(data=data)
+# serializer.is_valid()
+# # True
+# serializer.validated_data
+# # OrderedDict([('title', ''), ('code', 'print("hello, world")\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])
+# serializer.save()
+# # <Snippet: Snippet object
+
+
 class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
