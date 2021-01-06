d36d93c5fde70e6b3ffb3322e2d507ace4086c6e
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-06 16:10:24 +0530

    mixins implementation

--- status/views.py
+++ status/views.py
@@ -4,6 +4,8 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from status.serializers.serializers import StatusSerializer
 from .models import Status
+from rest_framework import mixins, generics, permissions
+from rest_framework.authentication import SessionAuthentication
 
 
 class StatusViewset(APIView):
@@ -15,6 +17,23 @@ class StatusViewset(APIView):
         serializer = StatusSerializer(qs, many=True)
 
         return JsonResponse(serializer.data, safe=False)
+
+
+class StatusAPIView(generics.ListAPIView, mixins.CreateModelMixin):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [SessionAuthentication]
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+    # if i change the func name from get to something else then the drf admin panel will show else normal web page is displayed
+    def getusers(self, request, format=None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    def post(self, request, *args, **kwargs):
+        print(request.user)
+        return self.create(request, *args, **kwargs)
 
 
 # Create your views here.
