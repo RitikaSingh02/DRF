34b8b54c8224a25b72d32247df8a2a88911014b8
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-06 16:10:11 +0530

    mixins implementation

--- status/urls.py
+++ status/urls.py
@@ -1,6 +1,8 @@
-from . views import StatusViewset
+from . views import StatusViewset, StatusAPIView
 from django.urls import include, path
 
 urlpatterns = [
-    path('crud/', StatusViewset.as_view())
+    path('crud/', StatusViewset.as_view()),
+    path('mixins/', StatusAPIView.as_view())
+
 ]
