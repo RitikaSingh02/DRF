3612212179d9c296a31aadcb93c9e690584a8c86
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-08 17:27:31 +0530

    payload modifications

--- status/JWT_MODIFICATIONS/jwt_custom_payload.py
+++ status/JWT_MODIFICATIONS/jwt_custom_payload.py
@@ -10,3 +10,9 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'email': user.email,
         'expiry_date': jwta["JWT_REFRESH_EXPIRATION_DELTA"]
     }
+
+
+'''
+what is a payload?:
+https://stackoverflow.com/questions/55069938/jsonwebtoken-what-is-payload#:~:text=In%20JSON%20Web%20Tokens%2C%20the,data%20for%20a%20particular%20user.&text=However%2C%20it%20can%20also%20contain,name%2C%20language%20preferences%2C%20etc.
+'''
