86424d5f522f6608a8397ab3ac3f92d06c4692c6
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-08 17:24:46 +0530

    aded mixins

--- status/urls.py
+++ status/urls.py
@@ -1,8 +1,9 @@
-from . views import StatusViewset, StatusAPIView
+from . views import StatusViewset, StatusAPIView, AuthView
 from django.urls import include, path
 
 urlpatterns = [
     path('crud/', StatusViewset.as_view()),
-    path('mixins/', StatusAPIView.as_view())
-
+    path('mixins/', StatusAPIView.as_view()),
+    path('jwt/', AuthView.as_view()),
+    # path('check/', check)
 ]
